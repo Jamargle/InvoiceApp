77693f4fc2714c09299f3f3675bc62f44dad2ef2
Jose <jmlb0003@gmail.com>
2023-12-28 18:18:40 +0100

    Remove shapes and typographies and BackButton for now since they are not needed

--- shared/src/androidMain/kotlin/com/jamarglex/invoiceapp/ui/theme/Theme.kt
+++ shared/src/androidMain/kotlin/com/jamarglex/invoiceapp/ui/theme/Theme.kt
@@ -1,49 +1,8 @@
 package com.jamarglex.invoiceapp.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Shapes
-import androidx.compose.material3.Typography
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-
-private val shapes = Shapes(
-    small = RoundedCornerShape(4.dp),
-    medium = RoundedCornerShape(4.dp),
-    large = RoundedCornerShape(0.dp)
-)
-
-// Set of Material typography styles to start with
-val typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
-)
 
 @Composable
 fun InvoiceAppTheme(
@@ -56,8 +15,6 @@ fun InvoiceAppTheme(
 
     MaterialTheme(
         colorScheme = colorScheme,
-        typography = typography,
-        shapes = shapes,
         content = content
     )
 }
--- shared/src/commonMain/kotlin/com/jamarglex/invoiceapp/ui/BackButton.kt
+++
@@ -1,9 +0,0 @@
-package com.jamarglex.invoiceapp.ui
-
-import androidx.compose.runtime.Composable
-
-@Composable
-expect fun BackButton(
-    backButtonText: String,
-    onBackClicked: () -> Unit
-)
